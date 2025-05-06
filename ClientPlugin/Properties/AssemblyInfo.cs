54adfbd5f7ce2e66497df6148f4f666bae34cf31
David Fernandes <davidsf@pantherify.com>
2025-05-06 21:29:08 +0300

    chore: Renaming

--- ClientPlugin/Properties/AssemblyInfo.cs
+++ ClientPlugin/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Sdx_se_direct_connect")]
+[assembly: AssemblyTitle("Se_direct_connect")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Sdx_se_direct_connect")]
+[assembly: AssemblyProduct("Se_direct_connect")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
